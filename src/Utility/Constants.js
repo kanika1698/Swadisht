d831330ec0d6ef06ff1a553cbc13ce93428d1194
kanika1698 <kanikakapur1698@gmail.com>
2023-08-17 21:53:49 +0530

    deploy

--- src/Utility/Constants.js
+++
@@ -1,5 +0,0 @@
-export const Image_Url =
-  "https://corsproxy.io/?https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
-export const Restaurant_data =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING";
